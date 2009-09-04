ee80d9e80e73ae639eafe2c1feda8b9bd28e1c47
Laurent Deniau <laurent.deniau@gmail.com>
2009-09-04 08:40:07 +0000

    Char vector added

---
+++ CosStd/include/cos/ChrVector.h
@@ -0,0 +1,118 @@
+#ifndef COS_CHRVECTOR_H
+#define COS_CHRVECTOR_H
+
+/*
+ o---------------------------------------------------------------------o
+ |
+ | COS Vector, Dynamic Vector, Lazy Vector and Vector View
+ |
+ o---------------------------------------------------------------------o
+ |
+ | C Object System
+ |
+ | Copyright (c) 2006+ Laurent Deniau, laurent.deniau@cern.ch
+ |
+ | For more information, see:
+ | http://cern.ch/laurent.deniau/cos.html
+ |
+ o---------------------------------------------------------------------o
+ |
+ | This file is part of the C Object System framework.
+ |
+ | The C Object System is free software; you can redistribute it and/or
+ | modify it under the terms of the GNU Lesser General Public License
+ | as published by the Free Software Foundation; either version 3 of
+ | the License, or (at your option) any later version.
+ |
+ | The C Object System is distributed in the hope that it will be
+ | useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ | of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ |
+ | See <http://www.gnu.org/licenses> for more details.
+ |
+ o---------------------------------------------------------------------o
+ |
+ | $Id: ChrVector.h,v 1.1 2009/09/04 08:40:05 ldeniau Exp $
+ |
+*/
+
+#include <cos/Vector.h>
+
+/* NOTE-USER: ChrVector class cluster constructors
+
+   see cos/Vector.h for description
+*/
+
+defclass(ChrVector, IntegralVector)
+  I8* value;
+  U32 size;
+  I32 stride;
+endclass
+
+// ----- automatic constructors
+
+#define aChrVector(...)     ( (OBJ)atChrVector    (__VA_ARGS__) )
+#define aChrVectorRef(...)  ( (OBJ)atChrVectorRef (__VA_ARGS__) )
+#define aChrVectorView(...) ( (OBJ)atChrVectorView(__VA_ARGS__) )
+
+// --- shortcuts
+
+#ifndef COS_NOSHORTCUT
+
+#define aSVec(...)  aChrVector(__VA_ARGS__)
+
+#endif
+
+/***********************************************************
+ * Implementation (private)
+ */
+
+// ----- Block vector
+
+defclass(ChrVector0, ChrVector) I8 _value[]; endclass
+defclass(ChrVector1, ChrVector) I8 _value[]; endclass
+defclass(ChrVector2, ChrVector) I8 _value[]; endclass
+defclass(ChrVector3, ChrVector) I8 _value[]; endclass
+defclass(ChrVector4, ChrVector) I8 _value[]; endclass
+defclass(ChrVector5, ChrVector) I8 _value[]; endclass
+defclass(ChrVector6, ChrVector) I8 _value[]; endclass
+defclass(ChrVector7, ChrVector) I8 _value[]; endclass
+defclass(ChrVector8, ChrVector) I8 _value[]; endclass
+defclass(ChrVector9, ChrVector) I8 _value[]; endclass
+defclass(ChrVectorN, ChrVector) I8 _value[]; endclass
+
+// ----- Fixed vector, Dynamic vector and Lazy vector
+
+defclass(ChrVectorFix, ChrVector)
+  I8* _value;
+  U32  capacity;
+endclass
+
+defclass(ChrVectorDyn, ChrVectorFix)
+endclass
+
+defclass(ChrVectorLzy, ChrVectorDyn)
+  I32 arity;
+  OBJ generator;
+endclass
+
+// ----- ChrVector view
+
+defclass(ChrVectorView, ChrVector)
+  struct ChrVector *ref;
+endclass
+
+// ----- allocators and initializers
+
+struct Slice;
+struct ChrVector* ChrVector_alloc(U32);
+struct ChrVector* ChrVectorView_init(struct ChrVectorView*, struct ChrVector*,
+                                     struct Slice*);
+
+// ----- automatic constructors
+
+#define atChrVector(...)              atVector    (Chr,I8,__VA_ARGS__)
+#define atChrVectorRef(buffer,size)   atVectorRef (Chr,buffer,size)
+#define atChrVectorView(vector,slice) atVectorView(Chr,vector,slice)
+
+#endif // COS_CHRVECTOR_H
---
+++ CosStd/src/ChrVector.c
@@ -0,0 +1,108 @@
+/*
+ o---------------------------------------------------------------------o
+ |
+ | COS ChrVector
+ |
+ o---------------------------------------------------------------------o
+ |
+ | C Object System
+ |
+ | Copyright (c) 2006+ Laurent Deniau, laurent.deniau@cern.ch
+ |
+ | For more information, see:
+ | http://cern.ch/laurent.deniau/cos.html
+ |
+ o---------------------------------------------------------------------o
+ |
+ | This file is part of the C Object System framework.
+ |
+ | The C Object System is free software; you can redistribute it and/or
+ | modify it under the terms of the GNU Lesser General Public License
+ | as published by the Free Software Foundation; either version 3 of
+ | the License, or (at your option) any later version.
+ |
+ | The C Object System is distributed in the hope that it will be
+ | useful, but WITHOUT ANY WARRANTY; without even the implied wstranty
+ | of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ |
+ | See <http://www.gnu.org/licenses> for more details.
+ |
+ o---------------------------------------------------------------------o
+ |
+ | $Id: ChrVector.c,v 1.1 2009/09/04 08:40:07 ldeniau Exp $
+ |
+*/
+
+#define    VECTOR_ONLY
+#define CHRVECTOR_ONLY
+
+#define T     ChrVector
+#define TM   mChrVector
+#define TP  pmChrVector
+#define TN    ChrVectorN
+#define TF    ChrVectorFix
+#define TD    ChrVectorDyn
+#define TL    ChrVectorLzy
+#define TV    ChrVectorView
+#define TS    "char vector"
+#define TE    Char
+
+#define TF1   IntFunction1
+#define TF2   IntFunction2
+#define TF3   IntFunction3
+#define TF4   IntFunction4
+#define TF5   IntFunction5
+
+#define PF1   I32FCT1
+#define PF2   I32FCT2
+#define PF3   I32FCT3
+#define PF4   I32FCT4
+#define PF5   I32FCT5
+
+#define VS    Vector.ValueSequence.Sequence
+
+#define VAL       I8
+#define VALINT  
+#define VALFLT  
+#define VALOBJ(v) aChar(v)
+
+#define  valref  value
+#define _valref _value
+
+#define aTView(v,s)    aChrVectorView(v,s)
+#define aTRef(v,s)     aChrVectorRef(v,s)
+
+#define T_alloc(s)      ChrVector_alloc(s)
+#define TV_init(vw,v,s) ChrVectorView_init(vw,v,s)
+
+#define TOVALPTR        gchrPtr
+#define TOVAL(v)        gchr(v)
+#define VALUE(v)        v->Int.value
+#define ASSIGN(d,s)     (d = s)
+#define SWAP(v1,v2)     { VAL tmp = v1; v1 = v2; v2 = tmp; }
+#define EQUAL(v1,v2)    (v1 == v2 ? True : False)
+#define COMPARE(v1,v2)  (v1<v2 ? Lesser : v1>v2 ? Greater : Equal)
+#define RETAIN(v)       (v)
+#define RELEASE(v)      
+#define AUTODELETE(v)   gautoDelete(v)
+#define PROTECT(v)   
+#define UNPROTECT(v) 
+
+#include <cos/ChrVector.h>
+#include <cos/Function.h>
+
+makclass(ChrVector, IntegralVector);
+
+// vector templates
+#include "./tmpl/Vector.c"
+
+#include "./tmpl/Vector_dyn.c"
+#include "./tmpl/Vector_lzy.c"
+#include "./tmpl/Vector_vw.c"
+
+#include "./tmpl/Vector_acc.c"
+#include "./tmpl/Vector_alg.c"
+#include "./tmpl/Vector_fun.c"
+#include "./tmpl/Vector_fct.c"
+#include "./tmpl/Vector_vfn.c"
+
